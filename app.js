2d96305dc35b1e321ad634cea6866a482755fd79
Juel Rei Gacusan <juelreigacusan22@gmail.com>
2023-08-11 20:58:17 +0800

    Another one

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
 const express = require('express')
 
 const app = express()
+const fs = require('fs')
+const path = require('path')
+
+function getImagesData(directory) {
+  return new Promise((resolve, reject) => {
+    fs.readdir(directory, (err, files) => {
+      if (err) return reject(err)
+
+      const imageFiles = files.filter((file) =>
+        ['.png', '.jpg', '.jpeg', '.gif', '.bmp', '.tiff'].includes(
+          path.extname(file).toLowerCase(),
+        ),
+      )
+
+      const imageData = imageFiles.map((file) => ({
+        imageRef: path.join(directory, file),
+        title: 'title',
+        colors: '8 colors',
+        price: '$200',
+        description:
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book',
+      }))
+
+      resolve(imageData)
+    })
+  })
+}
 
 // Set EJS as templating engine
 app.set('view engine', 'ejs')
@@ -10,7 +37,7 @@ app.use(require('express-ejs-layouts'))
 app.set('layout', './layouts/master')
 
 // Products page
-app.get('/products', (req, res) => {
+app.get('/products', async (req, res) => {
   const viewsData = {
     items: items,
     sizes: sizes,
--- constants/items.js
+++ constants/items.js
@@ -1,7 +1,10 @@
 const items = [
   {
+    id: 'item-1',
     imageRef:
       'https://images.unsplash.com/photo-1494232410401-ad00d5433cfa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+    imageRefSecondary:
+      'https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1839&q=80',
     title: 'Title',
     colors: '8 colors',
     price: '$200',
@@ -9,6 +12,9 @@ const items = [
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book',
   },
   {
+    id: 'item-2',
+    imageRefSecondary:
+      'https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1839&q=80',
     imageRef:
       'https://images.unsplash.com/photo-1587590227264-0ac64ce63ce8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
     title: 'Title 1',
@@ -18,6 +24,9 @@ const items = [
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book',
   },
   {
+    id: 'item-3',
+    imageRefSecondary:
+      'https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1839&q=80',
     imageRef: 'https://picsum.photos/200/300',
     title: 'Title 2',
     colors: '8 colors',
